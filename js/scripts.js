f2028f963b49173a91b7c42607303643e5320bf6
Reid Baldwin & Brett Moore <student@epicodus.com>
2015-04-08 10:06:45 -0700

    add functionality to our multiple pennies test

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,37 @@
+// var coinCounter = function(input) {
+//     var p = 1;
+//     var n = 5;
+//     var d = 10;
+//     var q = 25;
+//      if (input === 1) {
+//          return '1 penny';
+//         }
+//         else if (input === 5) {
+//             return '1 nickel';
+//         }
+//          else if (input === 10) {
+//             return '1 dime';
+//         }
+//         else if (input === 25) {
+//             return '1 quarter';
+//         }
+//         else if (input === p+n) {
+//             return '1 penny, 1 nickel';
+//         }
+//         else if (input === p+n+d) {
+//             return '1 penny, 1 nickel, 1 dime';
+//         }
+//         else if (input === p+n+d+q) {
+//             return '1 penny, 1 nickel, 1 dime, 1 quarter';
+//         }
+// };
+
 var coinCounter = function(input) {
-     if (input === 1) {
-         return '1 pennies';
-  }
-        else if (input === 5) {
-            return '1 nickel';
-        }
+    var p = 1;
+    var n = 5;
+    var d = 10;
+    var q = 25;
+    if (input <= 4) {
+        return input.toString().concat(' pennies');
+    }
 };
--- specs/specs.js
+++ specs/specs.js
@@ -1,9 +1,27 @@
 describe('coinCounter', function() {
     it("will display a penny if the user inputs 1", function() {
-        expect(coinCounter(1)).to.equal('1 pennies');
+        expect(coinCounter(1)).to.equal('1 penny');
     });
 
     it("will display a nickel if the user inputs 5", function() {
         expect(coinCounter(5)).to.equal('1 nickel');
+    });
+    it("will display a dime if the user inputs 10", function() {
+        expect(coinCounter(10)).to.equal('1 dime');
+    });
+    it("will display a quarter if the user inputs 25", function() {
+        expect(coinCounter(25)).to.equal('1 quarter');
+    });
+    it("will display a nickel and a penny if the user inputs 6", function() {
+        expect(coinCounter(6)).to.equal('1 penny, 1 nickel');
+    });
+    it("will display a nickel and a penny and a dime if the user inputs 16", function() {
+        expect(coinCounter(16)).to.equal('1 penny, 1 nickel, 1 dime');
+    });
+    it("will display a penny, a nickel, a dime, and a quarter if the user inputs 41", function () {
+        expect(coinCounter(41)).to.equal('1 penny, 1 nickel, 1 dime, 1 quarter');
+    });
+    it("will display 4 pennies if the user inputs 4", function() {
+        expect(coinCounter(4)).to.equal('4 pennies');
     });
 });
